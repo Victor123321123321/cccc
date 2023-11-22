e0dbeaccf55f2915aee435d0e75b7b0ecec62ee2
Victor123321123321 <KopytovVictor@mail.ru>
2023-11-22 23:11:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cccc/RestoreUnitTest.java
@@ -0,0 +1,38 @@
+package com.example.cccc;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RestoreUnitTest {
+    @Test
+    public void a() {
+        // Context of the app under test.
+        assertEquals("com.example.cccc", "123");
+    }
+    @Test
+    public void b() {
+        // Context of the app under test.
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void c() {
+        // Context of the app under test.
+        assertEquals("com.example.cccc", "123");
+    }
+    @Test
+    public void d() {
+        // Context of the app under test.
+        assertEquals("com.example.cccc", "123");
+    }
+    @Test
+    public void e() {
+        // Context of the app under test.
+        assertEquals("com.example.cccc", "123");
+    }
+    @Test
+    public void f() {
+        // Context of the app under test.
+        assertEquals("com.example.cccc", "123");
+    }
+}
